f3188a293a8bba8c964bc18b24df65f02e935b61
ElyPoli <itsos.poli@gmail.com>
2023-09-08 16:19:37 +0200

    create file - functions

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
-// Lunghezza password selezionata
-$password_length = $_GET["length"] ?? '0';
-
-// Creo i diversi array separati
-$alphabet_lowercase = range('a', 'z');
-$alphabet_uppercase = range('A', 'Z');
-$number = range(0, 9);
-$symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '<', '>', ',', '.', '?', '/'];
-
-// Unisco tutti gli array in uno unico
-$array_character_set = array_merge($alphabet_lowercase, $alphabet_uppercase, $number, $symbols);
-
-// Estraggo casualmente tot elementi dall'array totale
-$random_password_generated = '';
-for ($i = 0; $i < $password_length; $i++) {
-    $random_password_generated .= $array_character_set[rand(0, count($array_character_set) - 1)];
-}
+include __DIR__ . "/functions.php";
 ?>
 
 <!DOCTYPE html>
